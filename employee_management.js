b9c600264ba2d12e1c9f2f14ee9e2d6b2bbcf523
LuisVieira0701 <luisvieira@usf.edu>
2024-10-05 23:06:22 -0400

    T5- Create and Manage departments and employees

--- employee_management.js
+++ employee_management.js
@@ -14,18 +14,29 @@ class Player {
 
 //Task 2- Create Team to Manage Players and calculate salary
 class Team {
-    constructor(teamName, players) {
+    constructor(teamName, players = []) {
         this.teamName = teamName
         this.players = players;
     }
 
     addPlayer(player) {
-        this.players.push(player);
+        this.players.unshift(player);
     }
 
     getTeamSalary() {
         let totalTeamSalary = this.players.reduce((totalSalary, player) => totalSalary + player.salary, 0)
-        return (`The total Salary for ${this.teamName} is ${totalTeamSalary}`)
+        return totalTeamSalary
+    }
+
+    //Task 4- Handle Bonuses in the Team salary calculation
+    calculateTotalSalarywithBonus() {
+        let totalSalaryNoBonus = this.getTeamSalary()
+
+        let totalBonusSalary =this.players
+            .filter(player => player.position === 'Coach')
+            .reduce((bonusIndex, coach) => bonusIndex + coach.bonus, 0)
+
+        return totalSalaryNoBonus + totalBonusSalary
     }
 }
 
@@ -36,5 +47,24 @@ class Coach extends Player {
         this.bonus = bonus;
     }
 }
+
+//Task 5 Create and manage departments and employees
+const ManU = new Team ('Manchester United')
+const Liv = new Team ('Liverpool')
+
+const antony = new Player ('Antony', 140000, 'Winger', 'Manchester United')
+const mitoma = new Player ('Mitoma', 150000, 'Winger', 'Liverpool')
+const pep = new Coach ('Guardiola', 200000, 'Coach', 'Manchester United', 30000)
+const tenHag = new Coach ('Ten Hag', 220000, 'Coach', 'Liverpool', 50000)
+
+ManU.addPlayer(antony)
+ManU.addPlayer(pep)
+Liv.addPlayer(mitoma)
+Liv.addPlayer(tenHag)
+
+console.log(`Total Salary for Manchester United: ${ManU.getTeamSalary()}`);
+console.log(`Total Salary with coach bonus for Manchester United: ${ManU.calculateTotalSalarywithBonus()}`)
+console.log(`Total Salary for Liverpool: ${Liv.getTeamSalary()}`)
+console.log(`Total Salary with coach bonus for Liverpool: ${Liv.calculateTotalSalarywithBonus()}`)
 
 
